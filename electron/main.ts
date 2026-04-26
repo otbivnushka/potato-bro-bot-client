236182e5706246360dc8b5f07e7b150fb6c32e22
otbivnushka <stukm1900@gmail.com>
2026-04-26 15:03:47 +0300

    added icon

--- electron/main.ts
+++ electron/main.ts
@@ -1,13 +1,24 @@
 import { app, BrowserWindow } from 'electron';
+import { fileURLToPath } from 'url';
+import path from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 function createWindow() {
   const win = new BrowserWindow({
     width: 1200,
     height: 800,
+    title: 'Potato Bro Bot',
+    minWidth: 650,
+    minHeight: 600,
+    icon: path.join(__dirname, '../src/assets/icon.png'),
     webPreferences: {
       nodeIntegration: true,
     },
   });
+
+  win.setMenu(null);
 
   win.loadURL('http://localhost:5173');
 }
